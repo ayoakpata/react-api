77a3ff5ef2542d675bbc7c47512ee230ae0b29eb
ayoakpata <akpata.ayobami@gmail.com>
2020-03-30 22:52:09 -0700

    auth0 files added

---
+++ github-api-auth/src/Components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+
+class Header extends Component {
+  render() {
+    return (
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>Github Searcher</Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavItem href='#'>Log In</NavItem>
+        </Nav>
+      </Navbar>
+    );
+  }
+}
+
+export default Header;
